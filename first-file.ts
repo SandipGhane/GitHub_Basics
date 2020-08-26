722f4f56ab1f705f04818d9304cb322b2c603445
Sandiprghane <64368583+Sandiprghane@users.noreply.github.com>
2020-08-26 22:17:21 +0530

    without fast forwording

--- first-file.ts
+++ first-file.ts
@@ -1,15 +1,3 @@
 //changed file
 
-//bejsm,zmd
-
-//gfsjsf
-
-//hdfjvdl
-
-//jslifkeldk
-//scncowklsf
-//isnfcmlkd
-
-//sandip rangnath ghane ..development branch
-//kiran rangnath ghane....master branch
-//hsbjs
+//merge wwithout fast forwording...
